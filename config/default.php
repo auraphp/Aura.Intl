a411775fbd300f808e01cfc7fb6f3b688311524b
Paul M. Jones <pmjones88@gmail.com>
2013-01-29 09:45:16 -0600

    standardize config file

--- config/default.php
+++ config/default.php
@@ -1,25 +1,29 @@
 <?php
 /**
- * Autoloader information.
+ * Loader
  */
 $loader->add('Aura\Intl\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
 /**
- * Configuration defaults.
- */
-$di->params['Aura\Intl\FormatterLocator']['registry'] = [
-    'basic' => function () { return new Aura\Intl\BasicFormatter; },
-    'intl'  => function () { return new Aura\Intl\IntlFormatter; },
-];
-
-$di->params['Aura\Intl\TranslatorLocator']['locale'] = 'en_US';
-$di->params['Aura\Intl\TranslatorLocator']['factory'] = $di->lazyNew('Aura\Intl\TranslatorFactory');
-$di->params['Aura\Intl\TranslatorLocator']['formatters'] = $di->lazyNew('Aura\Intl\FormatterLocator');
-$di->params['Aura\Intl\TranslatorLocator']['packages'] = $di->lazyNew('Aura\Intl\PackageLocator');
-
-/**
- * Dependency services.
+ * Services
  */
 $di->set('intl_package_factory', $di->lazyNew('Aura\Intl\PackageFactory'));
 $di->set('intl_translator_locator', $di->lazyNew('Aura\Intl\TranslatorLocator'));
 
+/**
+ * Aura\Intl\FormatterLocator
+ */
+$di->params['Aura\Intl\FormatterLocator']['registry'] = [
+    'basic' => $di->lazyNew('Aura\Intl\BasicFormatter'),
+    'intl'  => $di->lazyNew('Aura\Intl\IntlFormatter'),
+];
+
+/**
+ * Aura\Intl\TranslatorLocator
+ */
+$di->params['Aura\Intl\TranslatorLocator'] = [
+     'locale' => 'en_US',
+    'factory' => $di->lazyNew('Aura\Intl\TranslatorFactory'),
+    'formatters' => $di->lazyNew('Aura\Intl\FormatterLocator'),
+    'packages' => $di->lazyNew('Aura\Intl\PackageLocator'),
+];
